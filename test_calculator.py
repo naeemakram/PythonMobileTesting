b8288c93c35b7a945423070e0b4a603ff6fdc732
Naeem Malik <naeem.akram.malik@gmail.com>
2026-01-31 23:36:42 +0500

    Add logging to tests
    Add proper logging to test scripts through logging method calls.

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,14 @@
 import json
+import logging
 import os
 from pages.calculator_page import CalculatorPage
 import pytest
 
+# 1. Initialize Logger
+LOGGER = logging.getLogger(__name__)
+
 def test_addition(driver):
-    
+    LOGGER.info(f"\nRunning Test Case: test_addition 2+2=4")
     # 1. Initialize the Page
     calc_page = CalculatorPage(driver)
     
@@ -20,7 +24,7 @@ def test_addition(driver):
 
 
 def test_multiplication(driver):
-    
+    LOGGER.info(f"\nRunning Test Case: test_multiplication 2*2=4")
     # 1. Initialize the Page
     calc_page = CalculatorPage(driver)
     
@@ -35,6 +39,7 @@ def test_multiplication(driver):
     assert result == "4"
 
 def test_subtraction(driver):
+    LOGGER.info(f"\nRunning Test Case: test_subtraction 4-2=2")
     calc_page = CalculatorPage(driver)
     calc_page.tap_digit("4")
     calc_page.tap_subtract()
@@ -44,6 +49,7 @@ def test_subtraction(driver):
     assert result == "2"
 
 def test_division(driver):
+    LOGGER.info(f"\nRunning Test Case: test_division 4/2=2")
     calc_page = CalculatorPage(driver)
     calc_page.tap_digit("4")
     calc_page.tap_divide()
@@ -59,7 +65,7 @@ def load_test_data():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     # Go up one level (..) to root, then into 'data'
     data_path = os.path.join(current_dir, "data", "test_data.json")
-    print(data_path)
+    LOGGER.info(f"Loading test data from: {data_path}")
 
     with open(data_path) as f:
         return json.load(f)
@@ -69,8 +75,8 @@ def test_addition_ddt(driver, data):
     """
     This test runs 3 times automatically (once for each block in the JSON).
     """
-    print(f"\nRunning Test Case: {data['test_name']}")
-    
+    LOGGER.info(f"Running Test Case: {data['test_name']}")
+    LOGGER.info(f"{data["input_a"]} + {data["input_b"]} = {data["expected"]}")
     calc_page = CalculatorPage(driver)
     
     # Use the dynamic data
